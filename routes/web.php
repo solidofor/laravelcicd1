05c50b5abc7c479fe6ddf26e066c9923ad907c20
solidofor <solidofor@yahoo.com>
2025-05-27 23:38:58 -0400

    welcome update

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,9 @@
 use App\Http\Controllers\MessageController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/welcome', function () {
+    return view('welcome');
+});
 
 
 Route::get('/', [MessageController::class, 'show']);
